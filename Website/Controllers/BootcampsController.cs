506fe3276fcc98043083384cc0a141958c4a20c5
Simon Gilquin <simon@gilquin.com>
2014-09-10 14:59:53 +0200

    Added list of bootcamps

---
+++ Website/Controllers/BootcampsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class BootcampsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public async Task<ActionResult> Index()
+        {
+            var bootcamps = await db.Bootcamps.ToListAsync();
+            return View(bootcamps);
+        }
+    }
+}
